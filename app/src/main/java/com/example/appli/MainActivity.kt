9e98bdd39d697ef27b2507b2c4b365b46cf910eb
Arghadip Banerjee <argha.ban55@gmail.com>
2019-08-13 20:14:09 +0530

    Second Layout

--- app/src/main/java/com/example/appli/MainActivity.kt
+++ app/src/main/java/com/example/appli/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.appli
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        submit.setOnClickListener {
+            val secondIntent = Intent(this, secondact::class.java)
+             startActivity(secondIntent)
+
+        }
+
+
     }
 }
