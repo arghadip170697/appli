4c91417b7969747dae318cd17b75aeeb75cb3134
Arghadip Banerjee <argha.ban55@gmail.com>
2019-08-13 23:27:46 +0530

    Second Layout

--- app/src/main/java/com/example/appli/fin.kt
+++ app/src/main/java/com/example/appli/fin.kt
@@ -3,17 +3,13 @@ package com.example.appli
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 
-class secondact : AppCompatActivity() {
+class fin : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_secondact)
+        setContentView(R.layout.activity_fin)
     }
-    fun button2(view: View){
-        val skillact = Intent(this, secondact::class.java)
-        startActivity(skillact)
 
     }
-}
+
