fd13f2ed5f37114045e98d6ed14a33af8045e179
Arghadip Banerjee <argha.ban55@gmail.com>
2019-08-13 18:20:19 +0530

    Second Layout

---
+++ app/src/main/java/com/example/appli/secondact.kt
@@ -0,0 +1,12 @@
+package com.example.appli
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class secondact : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_secondact)
+    }
+}
